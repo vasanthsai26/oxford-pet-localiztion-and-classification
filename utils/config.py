b778d75183ce395afc5d3d38e0f000b822ad620e
kalluri <vasanthsai26@gmail.com>
2023-02-23 23:13:14 +0530

    modified paths

--- utils/config.py
+++ utils/config.py
@@ -3,10 +3,10 @@ import os
 
 ## DIR_CONFIGS
 WORKING_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-INPUT_IMAGE_DIR   = os.path.join(WORKING_DIR,"static/input-images")
-OUTPUT_IMAGE_DIR  = os.path.join(WORKING_DIR,"static/output-images")
-TRAINED_MODEL_DIR = os.path.join(WORKING_DIR,"model/trained-model.pth")
-IDX_LABEL_JSON_DIR= os.path.join(WORKING_DIR,"static/idx_to_label.json")
+INPUT_IMAGE_DIR   = os.path.join(WORKING_DIR,"static","input-images")
+OUTPUT_IMAGE_DIR  = os.path.join(WORKING_DIR,"static","output-images")
+TRAINED_MODEL_DIR = os.path.join(WORKING_DIR,"model","trained-model.pth")
+IDX_LABEL_JSON_DIR= os.path.join(WORKING_DIR,"static","idx_to_label.json")
 
 ## DATA CONFIGS
 IMG_SIZE = 224
